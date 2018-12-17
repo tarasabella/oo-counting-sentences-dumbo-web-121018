b7cb0d6fc86c76f8a259920a36c3445573e6e956
Tara Sabella <tarasabella@gmail.com>
2018-12-17 02:21:24 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,44 @@
 require 'pry'
 
 class String
-
-  def sentence?
+  
+  attr_accessor :string
+  
+  @@all = []
+  #creates a class variable scoped within the String class 
+  
+ def initialize
+   
+   @@all << self
+   
+   #everytime a new string is created its shoeveled into the string class 
+ end  
+ 
+  def self.all 
+    @@all 
+    #returns an array of all string instances 
+  end 
     
+  def sentence? # => returns true if teh string ends in a question mark
+  #iterate through the string class and see if it ends in a period 
+  #self.all.select do |self|
+   self.end_with?(".")
   end
 
-  def question?
 
+  def question?
+self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
-  def count_sentences
+  def count_sentences # => returns # of sentences in a string 
+self.split("").count # => split the string on any puncttuation to get each elements 
+#count the numbeer of elements 
+self.count_sentences.compact
 
   end
-end
+end 
+end 
